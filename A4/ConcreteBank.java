df529e4db827e623fe418637844f9c37e4dc68a0
wcleungaj <90839737+RickyHKUST@users.noreply.github.com>
2022-05-16 22:21:17 +0800

    [PA4] singleThreadTest

--- A4/ConcreteBank.java
+++ A4/ConcreteBank.java
@@ -1,4 +1,83 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ConcreteBank implements Bank {
 // TODO
+	HashMap<String, Integer> accountMap = new HashMap<String, Integer>();
+	HashMap<String, ReentrantLock> LockMap = new HashMap<String, ReentrantLock>();
+	
+	@Override
+	public boolean addAccount(String accountID, Integer initBalance) {
+		// TODO Auto-generated method stub
+		if(accountMap.containsKey(accountID)) {
+			return false;
+		}
+		accountMap.put(accountID, initBalance);
+		LockMap.put(accountID, new ReentrantLock());
+		return true;
+	}
+
+	@Override
+	public boolean deposit(String accountID, Integer amount) {
+		// TODO Auto-generated method stub
+		if(!accountMap.containsKey(accountID))
+			return false;
+		accountMap.put(accountID, accountMap.get(accountID)+amount);
+		return true;
+	}
+
+	@Override
+	public boolean withdraw(String accountID, Integer amount, long timeoutMillis) {
+		// TODO Auto-generated method stub
+		if(!accountMap.containsKey(accountID)) {
+			return false;
+		}
+		
+		ReentrantLock lock = LockMap.get(accountID);
+		synchronized(lock) {
+			int balance = accountMap.get(accountID);
+			if(balance<amount) {
+				try {
+					lock.wait(timeoutMillis);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				balance = accountMap.get(accountID);
+			}
+			
+			if(balance>=amount) {
+				accountMap.put(accountID, balance-amount);
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean transfer(String srcAccount, String dstAccount, Integer amount, long timeoutMillis) {
+		// TODO Auto-generated method stub
+		if(withdraw(srcAccount,amount,timeoutMillis)) {
+			if(deposit(dstAccount,amount)) {
+				return true;
+				}
+			deposit(srcAccount,amount);
+		}
+		return false;
+	}
+
+	@Override
+	public Integer getBalance(String accountID) {
+		// TODO Auto-generated method stub
+		if(!accountMap.containsKey(accountID))
+			return 0;
+		return accountMap.get(accountID);
+	}
+
+	@Override
+	public void doLottery(ArrayList<String> accounts, Miner miner) {
+		// TODO Auto-generated method stub
+		
+	}
 }
