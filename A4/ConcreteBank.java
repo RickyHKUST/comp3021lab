0b2669adb72e1d76642d6b136e136f72476923b7
wcleungaj <90839737+RickyHKUST@users.noreply.github.com>
2022-05-20 22:46:52 +0800

    [PA4] Code refactoring
    submitted as A4.zip

--- A4/ConcreteBank.java
+++ A4/ConcreteBank.java
@@ -9,10 +9,7 @@ public class ConcreteBank implements Bank {
 	
 	@Override
 	public boolean addAccount(String accountID, Integer initBalance) {
-		// TODO Auto-generated method stub
-		if(accountMap.containsKey(accountID)) {
-			return false;
-		}
+		if(accountMap.containsKey(accountID)) {return false;}
 		accountMap.put(accountID, initBalance);
 		LockMap.put(accountID, new ReentrantLock());
 		return true;
@@ -20,11 +17,9 @@ public class ConcreteBank implements Bank {
 
 	@Override
 	public boolean deposit(String accountID, Integer amount) {
-		// TODO Auto-generated method stub
 		ReentrantLock lock = LockMap.get(accountID);
 		synchronized(lock) {
-			if(!accountMap.containsKey(accountID))
-				return false;
+			if(!accountMap.containsKey(accountID)) {return false;}
 			accountMap.put(accountID, accountMap.get(accountID)+amount);
 			lock.notifyAll();
 			return true;
@@ -33,56 +28,39 @@ public class ConcreteBank implements Bank {
 
 	@Override
 	public boolean withdraw(String accountID, Integer amount, long timeoutMillis) {
-		// TODO Auto-generated method stub
-		if(!accountMap.containsKey(accountID)) {
-			return false;
-		}
-		
+		if(!accountMap.containsKey(accountID)) {return false;}
 		ReentrantLock lock = LockMap.get(accountID);
 		synchronized(lock) {
 			int balance = accountMap.get(accountID);
 			if(balance<amount) {
-				try {
-					lock.wait(timeoutMillis);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+				try {lock.wait(timeoutMillis);}
+				catch (InterruptedException e) {e.printStackTrace();}
 				balance = accountMap.get(accountID);
 			}
-			
 			if(balance>=amount) {
 				accountMap.put(accountID, balance-amount);
 				return true;
 			}
 		}
-		
 		return false;
 	}
 
 	@Override
 	public boolean transfer(String srcAccount, String dstAccount, Integer amount, long timeoutMillis) {
-		// TODO Auto-generated method stub
 		if(withdraw(srcAccount,amount,timeoutMillis)) {
-			if(deposit(dstAccount,amount)) {
-				return true;
-				}
-			deposit(srcAccount,amount);
+			if(deposit(dstAccount,amount)) {return true;}
+			else {deposit(srcAccount,amount);}
 		}
 		return false;
 	}
 
 	@Override
 	public Integer getBalance(String accountID) {
-		// TODO Auto-generated method stub
-		if(!accountMap.containsKey(accountID))
-			return 0;
-		return accountMap.get(accountID);
+		return (accountMap.containsKey(accountID))?accountMap.get(accountID):0;
 	}
 
 	@Override
 	public void doLottery(ArrayList<String> accounts, Miner miner) {
-		// TODO Auto-generated method stub
-		
 		ArrayList<Thread> threads = new ArrayList<Thread>();
 		for(String account:accounts) {
 			Thread t = new Thread(()->{deposit(account, miner.mine(account));});
@@ -90,11 +68,8 @@ public class ConcreteBank implements Bank {
 			t.start();
 		}
 		for(Thread t:threads) {
-			try {
-				t.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			try {t.join();}
+			catch (InterruptedException e) {e.printStackTrace();}
 		}
 	}
 }
